8a2315aa6e009b94b11758d92afa67e625f2634b
Matt Mowers <matt.mowers@gmail.com>
2023-04-07 12:23:00 -0400

    Add error handling to postprocessing

--- custom_files/results.py
+++ custom_files/results.py
@@ -3,9 +3,9 @@ from pdb import set_trace as b
 import os
 
 results_folder = '/home/mmowers/GCAM/run_results_2023-03-07'
-outputs_folder = f'{results_folder}/csv_results'
+outputs_folder = f'{results_folder}/csv_results_lowshare'
 os.mkdir(outputs_folder)
-scens = ['ref_core','ref_plcoe', 'lowcarb_core', 'lowcarb_plcoe']
+scens = ['ref_plcoe_lowshare','lowcarb_plcoe_lowshare']
 filters = {
     'inputs by tech': {'sector':['electricity']},
     'prices of all markets': {'market':['USAwind-trial-supply', 'USAsolar-trial-supply','USAwind_offshore-trial-supply']},
@@ -14,15 +14,22 @@ filters = {
 }
 concat_dct = {} #key is the name of the output, and value is a list of dataframes to be concatenated (each scenario)
 for scen in scens:
-    print(f'Gathering results from {scen}')
+    print(f'\nGathering results from {scen}')
     df = pd.read_csv(f'{results_folder}/queryout_{scen}.csv', header=None, sep='\n')
+    #Remove results that didn't return results
     df = df[0].str.split(',', expand=True) #Unfortunately the raw data has [scenario],[date] in the "scenario" column. See HACK below
+    queryError = df[0].str.contains('The query returned no results')
+    if len(df[queryError]) > 0:
+        print('Query errors:')
+        print(df[queryError])
+        df = df[~queryError].reset_index(drop=True)
     df_nm = df[df[1].isnull()][0].copy() #series of table names and associated index
     df_nm[len(df)] = 'end' #Add the index of the end of the dataframe
     for i in range(len(df_nm) - 1): #minus 1 because the final entry is just 'end'
         df_res = df.iloc[df_nm.index[i]:df_nm.index[i+1],:].copy()
         #Find name of table and add it if it doesn't already exist
         name = df_res.iloc[0,0]
+        print(name)
         if name not in concat_dct:
             concat_dct[name] = []
         #Remove first row (with just the name)
@@ -49,6 +56,7 @@ for scen in scens:
         df_res = pd.melt(df_res, id_vars=id_vars, var_name='year', value_name='value')
         concat_dct[name].append(df_res)
 
+print('\nOutputting results')
 for name in concat_dct:
     print(f'Outputting {name}')
     output = pd.concat(concat_dct[name], ignore_index=True)
